df03b28759231aca4da120547004ad2267db2080
郭胜强 <guoshengqiang@live.com>
2017-11-22 23:38:37 +0800

    事件监听

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 var input
-document.addEventListener('touchstart', function(event) {
+document.addEventListener('touchend', updateInput)
+document.addEventListener('keydown', updateInput)
+function updateInput(event) {
     var target = event.target
     var tagName = target.tagName
-    if(tagName === 'INPUT' || tagName === 'TEXTAREA') {
+    // target === document.activeElement
+    if((tagName === 'INPUT' || tagName === 'TEXTAREA') && target !== input) {
+        console.log(target)
+        input && input.removeEventListener('keydown', kewdown, false)
         input = target
-        input.addEventListener('keydown', kewdown)
+        // kewdown(event)
+        input.addEventListener('keydown', kewdown, false)
     }
-})
+}
 function kewdown(event) {
-    event.preventDefault()
     console.log(event)
+    event.preventDefault()
+    var keyCode = event.keyCode
+    var key = show.key
+    switch(keyCode) {
+        case 8:
+            show.key = key.substr(0, key.length - 1)
+            break;
+        default:
+            if(event.key.match(/^[a-z]$/)) {
+                show.key += event.key
+            }
+            break;
+    }
+
+}
+var key = document.getElementById('key')
+var ul = document.getElementById('ul')
+var show = {
+    _key: '',
+    set key(data) {
+        this._key = data
+        key.innerText = data
+    },
+    get key() {
+        return this._key
+    }
 }

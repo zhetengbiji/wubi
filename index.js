f52772ef8dedf96f2681998b9e1f69d84fbc152e
郭胜强 <guoshengqiang@live.com>
2017-11-24 19:34:11 +0800

    获取文字

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 var input
 document.addEventListener('touchend', updateInput)
 document.addEventListener('keydown', updateInput)
+function getText(text, successCallback, errorCallback) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://ssssss.link/inputtools/request?text=' + text + '&itc=zh-t-i0-wubi-1986&num=13&cp=0&cs=1&ie=utf-8&oe=utf-8&app=demopage')
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+            if(xhr.status == 200) {
+                var res = xhr.responseText
+                var data
+                try {
+                    data = JSON.parse(res)
+                } catch(error) {
+                    console.error('error:', error)
+                    errorCallback && errorCallback(error)
+                }
+                console.log(data)
+                successCallback && successCallback(data)
+            } else {
+                console.error('error:', xhr.status)
+                errorCallback && errorCallback(xhr)
+            }
+        }
+    }
+    xhr.send(null)
+}
 function updateInput(event) {
     var target = event.target
     var tagName = target.tagName
@@ -37,6 +61,16 @@ var show = {
     set key(data) {
         this._key = data
         key.innerText = data
+        getText(data, function(text) {
+            var texts = text[1][0][1]
+            ul.innerHTML = ''
+            texts.forEach(element => {
+                var li = document.createElement('div')
+                li.classList.add('text-li')
+                li.innerText = element
+                ul.appendChild(li)
+            })
+        })
     },
     get key() {
         return this._key
